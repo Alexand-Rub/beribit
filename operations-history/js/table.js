eedee3a40512a2dcc4b5185689abffee78bfeaf7
Alexandr Rublev <alexandrublev@yandex.ru>
2022-05-11 14:05:22 +0300

    fixs added

---
+++ operations-history/js/table.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var element=document.querySelector('.table__value');
+    if(!element){
+        document.querySelector('.table__no-contect').classList.add('table__no-contect_active')
+    }
+
+})
+
+
