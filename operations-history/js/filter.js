524a8e0313944e75d5eb1a1083e2c539463d74a9
Alexandr Rublev <alexandrublev@yandex.ru>
2022-05-14 00:28:13 +0300

    filter list fixed

--- operations-history/js/filter.js
+++ operations-history/js/filter.js
@@ -6,18 +6,5 @@ document.addEventListener('DOMContentLoaded', () => { // Структура ст
         parent.classList.toggle('filter__item_active');
     })
   )
-  const button = document.querySelector('.filter__btn') // находим кнопку для открытия/закрытия окна навигации
-  const nav = document.querySelector('.filter__list') // находим окно навигации
-
-  button.addEventListener('click', () => { // при клике на кнопку
-    nav.classList.toggle('filter__list_active') // открываем/закрываем окно навигации, добаляя/удаляя активный класс
-  })
-
-  window.addEventListener('click', e => { // при клике в любом месте окна браузера
-    const target = e.target // находим элемент, на котором был клик
-    if (!target.closest('.filter__list') && !target.closest('.filter__btn')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-      nav.classList.remove('filter__list_active') // то закрываем окно навигации, удаляя активный класс
-    }
-  })
   
 })
