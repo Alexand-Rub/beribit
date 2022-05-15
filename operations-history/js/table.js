0d607915acc0b39e458d397b8c6ec751d6a3157b
Alexandr Rublev <alexandrublev@yandex.ru>
2022-05-15 04:19:51 +0300

    table scroll fixed

--- operations-history/js/table.js
+++ operations-history/js/table.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     var element=document.querySelector('.table__value');
     if(!element){
         document.querySelector('.table__no-contect').classList.add('table__no-contect_active')
-    }
+    };
+
+    
+    new SimpleBar(document.getElementById('table'), {
+        autoHide: false
+    });
 
 })
 
