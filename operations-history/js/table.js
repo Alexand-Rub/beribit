6dd48c96a66ffb8a72b8326aebf51045483f2b47
Alexandr Rublev <alexandrublev@yandex.ru>
2022-05-16 14:03:17 +0300

    loading and no-content added

--- operations-history/js/table.js
+++ operations-history/js/table.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var element=document.querySelector('.table__value');
-    if(!element){
-        document.querySelector('.table__no-contect').classList.add('table__no-contect_active')
-    };
-
     
     new SimpleBar(document.getElementById('table'), {
         autoHide: false
