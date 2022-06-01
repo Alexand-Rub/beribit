1b281e33488365e6846e0bdcf7e042de4a61eeb3
Alexandr Rublev <alexandrublev@yandex.ru>
2022-06-01 21:34:54 +0300

    table_none added

--- operations-history/js/table.js
+++ operations-history/js/table.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     new SimpleBar(document.getElementById('table'), {
         autoHide: false
     });
+
+    const table = document.querySelectorAll('.table__value')
+    console.log(table.length)
+    if (table.length == 0) {
+        document.querySelector('.main').classList.add('table_none')
+    }
 
 })
 
