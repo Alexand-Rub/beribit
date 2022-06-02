c45f297db194286a693beb6f4b6e444592886276
Alexandr Rublev <alexandrublev@yandex.ru>
2022-06-03 02:52:18 +0300

    details link fixed

--- operations-history/js/table.js
+++ operations-history/js/table.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     new SimpleBar(document.getElementById('table'), {
         autoHide: false
-    });
+    })
 
     const table = document.querySelectorAll('.table__value')
+    
     console.log(table.length)
     if (table.length == 0) {
         document.querySelector('.main').classList.add('table_none')
