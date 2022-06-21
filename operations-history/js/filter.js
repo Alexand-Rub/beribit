c932d7e7cce7857e4bdf8812f8c5227d7fafbce0
Alexandr Rublev <alexandrublev@yandex.ru>
2022-06-21 06:57:53 +0300

    pages anf filter fixed

--- operations-history/js/filter.js
+++ operations-history/js/filter.js
@@ -5,6 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
             parent.classList.toggle('filter__item_active');
         })
     );
+    document.querySelectorAll('.filter__open').forEach((item) =>
+        item.addEventListener('click', () => {
+            const parent = item.parentNode;
+            parent.classList.toggle('filter_open');
+        })
+    );
+
+    const parentElement = document.getElementById('filter');
+    let ua = navigator.userAgent,
+        event = (ua.match(/iPad/i) || ua.match(/iPhone/)) ? "touchstart" : "click";
+
+    document.addEventListener(event, (e) => {
+    let target = e.target;
+    
+    if (!(parentElement.contains(target))) {
+        parentElement.classList.remove('filter_open')
+    }
+    })
 });
 
 var a,b;
